75ae5fab8595f64136bd6d1feb07cc1c610708ed
kumavis <kumavis@users.noreply.github.com>
2023-03-07 11:16:08 -1000

    Initial commit

---
+++ packages/site/src/components/Account.tsx
@@ -0,0 +1,84 @@
+import { type KeyringAccount } from '@metamask/keyring-api';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import React, { useState } from 'react';
+
+import { MethodButton } from './Buttons';
+import { CopyableItem } from './CopyableItem';
+import {
+  AccountContainer,
+  AccountTitleContainer,
+  AccountTitle,
+  AccountTitleIconContainer,
+  AccountRow,
+  AccountRowTitle,
+  AccountRowValue,
+} from './styledComponents';
+
+export const Account = ({
+  account,
+  handleDelete,
+}: {
+  account: KeyringAccount;
+  handleDelete: (accountId: string) => Promise<void>;
+}) => {
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
+  return (
+    <AccountContainer>
+      <AccountTitleContainer>
+        <AccountTitle>{account.address}</AccountTitle>
+        <AccountTitleIconContainer>
+          {isCollapsed ? (
+            <ExpandMoreIcon
+              fontSize="large"
+              onClick={() => setIsCollapsed(!isCollapsed)}
+            />
+          ) : (
+            <ExpandLessIcon
+              fontSize="large"
+              onClick={() => setIsCollapsed(!isCollapsed)}
+            />
+          )}
+        </AccountTitleIconContainer>
+      </AccountTitleContainer>
+      {!isCollapsed && (
+        <>
+          <AccountRow>
+            <AccountRowTitle>ID</AccountRowTitle>
+            <CopyableItem value={account.id} />
+          </AccountRow>
+          <AccountRow>
+            <AccountRowTitle>Address</AccountRowTitle>
+            <CopyableItem value={account.address} />
+          </AccountRow>
+          <AccountRow>
+            <AccountRowTitle>Type</AccountRowTitle>
+            <AccountRowValue>{account.type}</AccountRowValue>
+          </AccountRow>
+          <AccountRow>
+            <AccountRowTitle>Account Supported Methods</AccountRowTitle>
+            <ul style={{ padding: '0px 0px 0px 16px' }}>
+              {account.methods.map((method, methodIndex) => (
+                <AccountRowValue
+                  key={`account-${account.id}-method-${methodIndex}`}
+                >
+                  <li>{method}</li>
+                </AccountRowValue>
+              ))}
+            </ul>
+          </AccountRow>
+        </>
+      )}
+      <AccountRow alignItems="flex-end">
+        <MethodButton
+          width="30%"
+          onClick={async (): Promise<void> => {
+            await handleDelete(account.id);
+          }}
+          label="Delete"
+        />
+      </AccountRow>
+    </AccountContainer>
+  );
+};
---
+++ packages/snap/src/stateManagement.ts
@@ -0,0 +1,39 @@
+import type { KeyringState } from './keyring';
+
+/**
+ * Default keyring state.
+ */
+const defaultState: KeyringState = {
+  wallets: {},
+  pendingRequests: {},
+  useSyncApprovals: true,
+};
+
+/**
+ * Retrieves the current state of the keyring.
+ *
+ * @returns The current state of the keyring.
+ */
+export async function getState(): Promise<KeyringState> {
+  const state = (await snap.request({
+    method: 'snap_manageState',
+    params: { operation: 'get' },
+  })) as any;
+
+  return {
+    ...defaultState,
+    ...state,
+  };
+}
+
+/**
+ * Persists the given snap state.
+ *
+ * @param state - New snap state.
+ */
+export async function saveState(state: KeyringState) {
+  await snap.request({
+    method: 'snap_manageState',
+    params: { operation: 'update', newState: state },
+  });
+}
