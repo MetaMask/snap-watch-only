aaa8dabd316e27f4020a01be58fa60522d072fc9
Kate Johnson <johnson.kat.g@gmail.com>
2023-10-18 15:20:17 -0400

    Working rejection of signing for keyring accounts

---
+++ packages/snap/src/test/watchOnlyKeyring.test.ts
@@ -0,0 +1,46 @@
+import { SimpleKeyring } from '../keyring';
+
+describe('SimpleKeyring', () => {
+  let keyring: SimpleKeyring;
+
+  beforeEach(() => {
+    keyring = new SimpleKeyring({
+      wallets: {
+        '0x1': {
+          account: {
+            id: '0x1',
+            address: '0x1',
+            type: 'eip155:eoa',
+            options: {},
+            methods: [],
+          },
+          privateKey: '0x1',
+        },
+      },
+      pendingRequests: {},
+      useSyncApprovals: false,
+    });
+  });
+
+  it('should not allow the user to sign transactions', async () => {
+    const result = await keyring.submitRequest({
+      id: '0x1',
+      request: {
+        method: 'eth_signTransaction',
+        params: [{ from: '0x1', to: '0x2', value: '0x1' }],
+      },
+      scope: '',
+      account: '',
+    });
+
+    expect(result).toStrictEqual({
+      pending: false,
+      result: null,
+      error: {
+        code: -32601,
+        message: 'Method not found',
+        data: null,
+      },
+    });
+  });
+});
