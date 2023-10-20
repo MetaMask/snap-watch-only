d4b609c2f7019a70269073d050b52ff5728c51b7
Kate Johnson <johnson.kat.g@gmail.com>
2023-10-19 20:53:16 -0400

    Moved ens functions into separate file

--- packages/site/src/utils/metamask.ts
+++ packages/site/src/utils/metamask.ts
@@ -1,5 +1,3 @@
-import { ethers } from 'ethers';
-
 /**
  * Detect if MetaMask is installed.
  *
@@ -7,36 +5,4 @@ import { ethers } from 'ethers';
  */
 export const hasMetaMask = async () => {
   return typeof window.ethereum !== 'undefined';
-};
-
-/**
- * Resolve ENS name to Ethereum address.
- *
- * @param name - ENS name to resolve.
- * @returns Ethereum address.
- */
-export const resolveName = async (name: string): Promise<string | null> => {
-  const provider = new ethers.BrowserProvider(window.ethereum);
-  try {
-    return await provider.resolveName(name);
-  } catch (error) {
-    console.error('Failed to resolve ENS name:', error);
-    return null;
-  }
-};
-
-/**
- * Lookup ENS name.
- *
- * @param address - Ethereum address to lookup.
- * @returns ENS name or `null` if not found.
- */
-export const lookupName = async (address: string): Promise<string | null> => {
-  const provider = new ethers.BrowserProvider(window.ethereum);
-  try {
-    return await provider.lookupAddress(address);
-  } catch (error) {
-    console.error('Failed to lookup ENS name:', error);
-    return null;
-  }
 };
