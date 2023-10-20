bfe4aeee75547d76e242393b1d8e1eb0b1d514a1
Kate Johnson <johnson.kat.g@gmail.com>
2023-10-19 23:44:05 -0400

    Added better type safety for Accordian method inputs

--- packages/site/src/types/inputs.ts
+++ packages/site/src/types/inputs.ts
@@ -1,4 +1,31 @@
+import type React from 'react';
+
 export enum InputType {
   Dropdown = 'dropdown',
   TextField = 'text-field',
 }
+
+export type InputField = {
+  id: string;
+  title: string;
+  type: InputType;
+  placeholder: string;
+  value?: any;
+  options?: { value: string }[];
+  onChange: (event: React.ChangeEvent) => void | Promise<void>;
+};
+
+export type Action = {
+  callback: () => Promise<any>;
+  label: string;
+  disabled?: boolean;
+  enabled?: boolean;
+};
+
+export type ManagementMethod = {
+  name: string;
+  description: string;
+  inputs?: InputField[];
+  action: Action;
+  successMessage?: string;
+};
