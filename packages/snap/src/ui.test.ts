62abea791a9f6de9b73d71030e8ca9437d94ece3
Kate Johnson <johnson.kat.g@gmail.com>
2024-03-18 16:23:52 -0400

    init: onRpcRquest and onHomePage tests

---
+++ packages/snap/src/ui.test.ts
@@ -0,0 +1,37 @@
+import { beforeEach, expect } from '@jest/globals';
+import type { Snap } from '@metamask/snaps-jest';
+import { installSnap } from '@metamask/snaps-jest';
+
+import { generateWatchFormComponent } from './ui/components';
+
+let snap: Snap;
+
+describe('Watch-only snap interactive UI', () => {
+  beforeEach(async () => {
+    snap = await installSnap();
+  });
+
+  describe('onRpcRequest', () => {
+    it('throws an error if the requested method does not exist', async () => {
+      const unsupportedMethod = 'foo';
+      const response = await snap.request({
+        method: unsupportedMethod,
+      });
+
+      expect(response).toRespondWithError({
+        code: -32603,
+        message: `Origin 'https://metamask.io' is not allowed to call '${unsupportedMethod}'`,
+        stack: expect.any(String),
+      });
+    });
+  });
+
+  describe('onHomePage', () => {
+    it('returns custom UI with the watch account form', async () => {
+      const ui = await snap.onHomePage();
+      const watchForm = generateWatchFormComponent();
+
+      expect(ui).toRender(watchForm);
+    });
+  });
+});
